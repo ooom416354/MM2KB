8f03e0729e8ed2cd6b72d99b32a1fbf2802ee950
Ken Balestrieri <nyxyanks@yahoo.com>
2017-02-09 02:00:56 -0500

    Weather

--- modules/default/currentweather/currentweather.js
+++ modules/default/currentweather/currentweather.js
@@ -18,7 +18,7 @@ Module.register("currentweather",{
 		updateInterval: 10 * 60 * 1000, // every 10 minutes
 		animationSpeed: 1000,
 		timeFormat: config.timeFormat,
-		showPeriod: true,
+		showPeriod: false,
 		showPeriodUpper: false,
 		showWindDirection: true,
 		useBeaufort: true,
